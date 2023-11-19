3973c7ed3b0e0b4f1abc6659ab07cb96f31a5477
Daniel Yang <dyang165@usc.edu>
2023-11-18 22:07:56 -0800

    Add in vt5 training

--- models/vt5.py
+++ models/vt5.py
@@ -4,9 +4,10 @@ import transformers
 from transformers import ViltModel, ViltProcessor, T5ForConditionalGeneration, T5TokenizerFast
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class VT5(nn.Module):
-    def __init__(self, freeze_vlm = True):
+    def __init__(self, vilt_chkpt = None, freeze_vlm = True):
         super().__init__()
 
         vilt_hf_path = 'dandelin/vilt-b32-finetuned-vqa'
@@ -14,6 +15,13 @@ class VT5(nn.Module):
 
         self.vilt_processor = ViltProcessor.from_pretrained(vilt_hf_path)
         self.vilt_model= ViltModel.from_pretrained(vilt_hf_path) # Replace this eventually
+        if vilt_chkpt != None:
+            ft_model = torch.load(vilt_chkpt)
+            if isinstance(ft_model, nn.DataParallel):
+                state_dict = ft_model.module.state_dict()
+            else:
+                state_dict = ft_model.state_dict()
+            self.vilt_model.load_state_dict(state_dict)
         self.t5_model = T5ForConditionalGeneration.from_pretrained(t5_hf_path)
         self.t5_processor = T5TokenizerFast.from_pretrained(t5_hf_path)
 
@@ -33,8 +41,9 @@ class VT5(nn.Module):
         # Project outputs into right dimension
         x = self.embedding_proj(x)
 
-        targets = self.t5_processor(questions, padding='longest', return_tensors='pt')['input_ids'].to(device)
+        targets = self.t5_processor(questions, padding=True, max_length=96, return_tensors='pt')['input_ids'].to(device)
         targets[targets == self.t5_processor.pad_token_id] = -100 # Remove padding from loss
+        print(targets.shape)
 
         # Generation
         outputs = self.t5_model(inputs_embeds = x,  labels=targets)
@@ -55,8 +64,8 @@ if __name__ == '__main__':
 
     url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     image = Image.open(requests.get(url, stream=True).raw)
-    question = "How many cats are there?"
+    question = "How many cats are there? There are four dogs."
     answer = '2'
 
     output = model(image, question, answer, sample=True)
-    print(output['loss'], output['text'])
+    print(output['loss'], output['text'], output['logits'].shape)
---
+++ train_vt5.py
@@ -0,0 +1,81 @@
+import itertools
+import torch
+import os
+import numpy as np
+import pandas as pd
+from models.vt5 import VT5
+from torch.utils.data import DataLoader
+from torch.nn.parallel import DataParallel
+from data.vilt_finetune_data_loader import ViltFinetuneDataLoader
+from data.vqg_dataset import VQGDataset
+from transformers import ViltConfig, ViltProcessor, ViltForQuestionAnswering, DefaultDataCollator
+from PIL import Image
+from tqdm.auto import tqdm
+from nltk.translate.bleu_score import corpus_bleu
+import wandb
+
+enable_wandb = True
+if enable_wandb:
+    wandb.login()
+    run_name = 'debug1'
+    wandb.init(project = 'vqg', name = 'debug1')
+
+saved_dataset_dir = 'saved_datasets/'
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+print(device)
+verbose = False
+
+# uses vilt config to check vocab
+
+config = ViltConfig.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+dataset = ViltFinetuneDataLoader.from_saved_or_load(config=config, verbose=verbose, force_reload=False, save_dir=saved_dataset_dir)
+print(f'loaded dataset with {len(dataset)} examples')
+dataset = VQGDataset(dataset, config)
+
+# Do we need this for data parallel?
+#vl_processor = ViltProcessor.from_pretrained('dandelin/vilt-b32-finetuned-vqa')
+#t5_processor = T5TokenizerFast.from_pretrained('t5-base')
+
+model = VT5()
+
+device_ids = [0]
+model = DataParallel(model, device_ids=device_ids)
+model.to(device)
+
+def collate_fn(x):
+    images = [item[0] for item in x]
+    questions = [item[1] for item in x]
+    answers = [item[2] for item in x]
+    return images, questions, answers
+train_dataloader = DataLoader(dataset,batch_size=64, collate_fn = collate_fn, shuffle=True)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+
+model.train()
+for epoch in range(50): 
+    print(f"Epoch: {epoch}")
+    for images, questions, answers in tqdm(train_dataloader):
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(images, questions, answers, sample=True)
+            loss = outputs['loss']
+            text = outputs['text']
+            
+            loss.backward()
+            optimizer.step()
+             
+            metrics = {'train_ce': loss.item()}
+            if enable_wandb:
+                wandb.log(metrics)
+    # TODO: Compute bleu on dev set
+    weights = [(1, 0, 0, 0),
+                (1./2., 1./2., 0, 0),
+                (1./3., 1./3., 1./3., 0),
+                (1./4., 1./4., 1./4., 1./4.)]
+    torch.save(model,f'checkpoints/{epoch}.pt')
