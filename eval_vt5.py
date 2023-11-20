Fix BLEU score computation

eval_vt5.py
@@ -19,7 +19,7 @@ import random
 from utils import get_nps
 random.seed(101)
 
-model_path = './checkpoints/0.pt'
+model_path = './checkpoints/1.pt'
 saved_dataset_dir = 'saved_dataset_val/'
 add_disjunctive_constraints=True
 
@@ -77,14 +77,16 @@ with torch.no_grad():
             for question in questions:
                 constraints+=[get_nps(question)]
         outputs = model.generate(images, answers, force_toks = constraints)
+        #outputs = model.generate(images, answers, force_toks=None)
         texts = outputs['text']
         pred.extend(texts)
         gt.extend(questions)
         
         idx+=1
-        #if idx == 1:
-    #    break
+        if idx == 10:
+            break
 
+print(pred[:10], gt[:10])
 tokenizer = AutoTokenizer.from_pretrained('t5-base')
 
 def tokenize(sentence):
@@ -98,7 +100,7 @@ for candidate, reference in zip(pred, gt):
     candidate_tok = tokenize(candidate)
     reference_tok = tokenize(reference)
     print(reference_tok, candidate_tok)
-    references.append(reference_tok)
+    references.append([reference_tok])
     candidates.append(candidate_tok)
 
 weights = [(1, 0, 0, 0),

models/vt5.py
@@ -103,7 +103,7 @@ class VT5(nn.Module):
                 except ValueError:
                     # In the rare case that a word gets split into subwords and there are repeated subwords
                     # we will just ignore it    
-                    pred_dict = self.t5_model_full.generate(inputs_embeds = seq_slice, 
+                    pred_dict = self.t5_model.generate(inputs_embeds = seq_slice, 
                                                             num_beams=self.beam_size, 
                                                             force_words_ids = None,
                                                             output_hidden_states=True,

train_vt5.py
@@ -85,4 +85,4 @@ for epoch in range(50):
                 (1./2., 1./2., 0, 0),
                 (1./3., 1./3., 1./3., 0),
                 (1./4., 1./4., 1./4., 1./4.)]
-    torch.save(model,f'checkpoints/{epoch}.pt')
+    torch.save(model,f'checkpoints/{run_name}-{epoch}.pt')